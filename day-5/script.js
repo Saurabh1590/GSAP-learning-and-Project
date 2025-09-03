d3f393f9d78aac0993ee5fee87418752b10f0b53
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-09-03 18:14:26 +0530

    day-5 Timeline_Animation

---
+++ day-5/script.js
@@ -0,0 +1,28 @@
+var cross = document.querySelector("#full i");
+var menu = document.querySelector("#nav i");
+
+var tl = gsap.timeline();
+
+tl.to("#full", {
+  right: 0,
+  duration: 0.6,
+});
+tl.from("#full h4", {
+  x: 150,
+  duration: 0.7,
+  stagger: 0.28,
+  opacity: 0,
+});
+tl.from(cross, {
+  opacity: 0,
+});
+
+tl.pause();
+
+menu.addEventListener("click",() => {
+    tl.play();
+})
+
+cross.addEventListener("click",() => {
+    tl.reverse();
+})
